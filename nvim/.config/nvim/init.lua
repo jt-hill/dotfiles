6c8e35fbe57d1b5d1e5066cea71b76cd5634105e
jt-hill <13084189+jt-hill@users.noreply.github.com>
2025-08-10 22:20:48 -0400

    copy nvim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.clipboard = "unnamedplus"
+-- vim.keymap.set("n", "<leader>a", function() print "leader works" end)
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- key mappings
+vim.api.nvim_set_keymap("i", "kj", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>a", function() print "leader works" end)
+
+-- vscode stuff
+if vim.g.vscode then
+    require("vscode")
+else
+-- plugins and other functionality for standalone use
+	vim.o.tabstop = 4
+	vim.o.shiftwidth = 4
+	vim.o.expandtab = true
+	vim.o.ignorecase = true
+	vim.o.smartcase = true
+    --require("kickstart")
+end
+
---
+++ nvim/.config/nvim/vscode.lua
@@ -0,0 +1,7 @@
+-- settings for vscode nvim plugin
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.ruler = false
+vim.o.showcmd = false
+vim.cmd("syntax off")
